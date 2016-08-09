005b36e4095e1994e662eced3f079ce6851948fd
Yu Yongwoo <uyu423@gmail.com>
2016-08-09 18:16:57 +0900

    Issue #15 #16 : React Application Reset. remove redux, react-router, etc..

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Panel, Button, Col, Row } from 'react-bootstrap';
+import { Row, Col, Panel, Button } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
 
 class Header extends React.Component {
 	render() {
-		return(
+		return (
 			<div className="page-header">
 				<Row>
-				<Col sm={6} smOffset={3}>
-				<Panel>
-					<Button bsStyle="primary" bsSize="large" block>
-						<FontAwesome name="sign-in" /> Login
-					</Button>
-				</Panel>
-				</Col>
+					<Col sm={6} smOffset={3}>
+						<Panel>
+							<Button className="center-block" bsSize="large" bsStyle="primary">
+								<FontAwesome name="sign-in"/> 로그인
+							</Button>
+						</Panel>
+					</Col>
 				</Row>
 			</div>
 		);
