994e05a696df61604f1616a7433c920c91909667
Yu Yongwoo <uyu423@gmail.com>
2016-08-09 21:45:18 +0900

    Issue #15 #16 : apply redux & react-redux, Login/Result Modal

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Row, Col, Panel, Button } from 'react-bootstrap';
 import FontAwesome from 'react-fontawesome';
+import { connect } from 'react-redux';
+import { loginModalOn } from '../actions';
+
+import LoginModal from './modals/LoginModal';
 
 class Header extends React.Component {
 	render() {
@@ -9,15 +13,22 @@ class Header extends React.Component {
 				<Row>
 					<Col sm={6} smOffset={3}>
 						<Panel>
-							<Button className="center-block" bsSize="large" bsStyle="primary">
+							<Button onClick={ this.props.onModal } className="center-block" bsSize="large" bsStyle="primary">
 								<FontAwesome name="sign-in"/> 로그인
 							</Button>
 						</Panel>
 					</Col>
 				</Row>
+			<LoginModal />
 			</div>
 		);
 	}
 }
 
-export default Header;
+let mapDispatchToProps = (dispatch) => {
+	return {
+		onModal : () => dispatch(loginModalOn())
+	}
+}
+
+export default connect(undefined, mapDispatchToProps)(Header);
--- app/index.js
+++ app/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import App from './container/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import voteApp from './reducers';
+
+const store = createStore(voteApp, window.devToolsExtension && window.devToolsExtension());
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+	<Provider store = {store}>
+		<App /> 
+	</Provider>,
+	rootElement
+);
