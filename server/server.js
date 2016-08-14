7c651e3d5d8f432c46d1f9106e9522e1036428f9
Yu Yongwoo <uyu423@gmail.com>
2016-08-15 01:18:14 +0900

    Issue #18 : production server config fixed

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
-app.use('/', express.static(path.join(__dirname, './..public')));
+app.use('/', express.static(path.join(__dirname, '../public')));
 app.use('/api', route);
 app.use('/assets', express.static(path.join(__dirname, '../bower_components')));
 app.get('*', (req, res) => {
